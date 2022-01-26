c441abfaaacb0b1c877f56c376622a52ade2cc5e
Cook21 <null>
2022-01-26 12:31:35 +0800

    Initial Commit

---
+++ iOS-App-Dev-Tutorials-Scrumdinger/iOS_App_Dev_Tutorials_ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_App_Dev_Tutorials_ScrumdingerApp.swift
+//  iOS-App-Dev-Tutorials-Scrumdinger
+//
+//  Created by Larry Xia on 2022/1/26.
+//
+
+import SwiftUI
+
+@main
+struct iOS_App_Dev_Tutorials_ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
